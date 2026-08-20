ac2bc57f45450a393115d9ff92e0605a7b24effa
askednobody410-web <askednobody410@gmail.com>
2026-08-20 17:23:27 +0200

    Add ESP script for fruit detection and notifications
    This script detects fruit objects in the game and creates ESP (Extra Sensory Perception) visuals for them. It also sends notifications to the user when the script is loaded and when fruits are detected.

---
+++ ESP/fruitesp.lua
@@ -0,0 +1,71 @@
+local plr = game:GetService("Players").LocalPlayer
+local char = game.Workspace.Characters:FindFirstChild(plr.Name)
+
+if not char then
+    for _, v in pairs(game.Workspace.Characters:GetChildren()) do
+        if v.Name == plr.Name then
+            char = v
+        end
+    end
+end
+
+local function notifyuser()
+    local startergui = game:GetService("StarterGui")
+    startergui:SetCore("SendNotification", {
+    Title = "Script loaded succesfully.",
+    Text = "made by tuxsaeht"
+    })
+end
+
+local function sendnotif(target)
+    local startergui = game:GetService("StarterGui")
+    startergui:SetCore("SendNotification", {
+    Title = "Fruit detected!",
+    Text = "A " .. target.Name .. " was detected!",
+    Duration = 5
+    })
+end
+
+local function esp(target)
+    local hrp = char.HumanoidRootPart
+    if not hrp then warn("Character doesn't exist!") return end
+    local a0 = hrp:FindFirstChild("Attachment0")
+    if not a0 then
+        a0 = Instance.new("Attachment")
+        a0.Name = "Attachment0"
+        a0.Parent = hrp
+    end
+    local a1 = target:WaitForChild("Handle",1):FindFirstChild("Attachment1")
+    if not a1 then
+        a1 = Instance.new("Attachment")
+        a1.Name = "Attachment1"
+        a1.Parent = target:WaitForChild("Handle",1)
+    end
+    local tracer = game.Workspace:FindFirstChild("Tracer_" .. target.Name)
+    if not tracer then
+        tracer = Instance.new("Beam")
+        tracer.Parent = game.Workspace
+        tracer.Name = "Tracer_" .. target.Name
+        tracer.Attachment0 = a0
+        tracer.Attachment1 = a1
+        tracer.Width0 = 0.25
+        tracer.Width1 = 0.25
+        tracer.FaceCamera = true
+    end
+end
+
+for _, v in pairs(game.Workspace:GetChildren()) do
+    if string.find(v.Name, "Fruit") then
+        esp(v)
+        sendnotif(v)
+    end
+end
+
+game.Workspace.ChildAdded:Connect(function(v)
+    if string.find(v.Name, "Fruit") then
+        esp(v)
+        sendnotif(v)
+    end
+end)
+
+notifyuser()
